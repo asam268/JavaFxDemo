29738678843b3b1e4beff1890b3df9553e5cbb4a
asam268 <asam268@gmail.com>
2020-10-06 17:51:51 -0400

    Create AlertBox

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     Stage window;
+    Button button;
     Scene scene1, scene2;
 
     public static void main(String[] args) {
@@ -20,26 +21,15 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         window = stage;
+        window.setTitle("Title of the Window");
 
-        Label label1 = new Label("Welcoome to the first scene");
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction(e -> window.setScene(scene2));
+        button = new Button("Click Me");
+        button.setOnAction(e -> AlertBox.display("Alert Title", "This is the message of the alert box."));
 
-        // Layout 1 - children are laid out in a vertical column
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 200, 200);
-
-        Button button2 = new Button("Go to scene 1");
-        button2.setOnAction(e -> window.setScene(scene1));
-
-        // Layout 2
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2, 600, 300);
-
-        window.setScene(scene1);
-        window.setTitle("Title here");
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
         window.show();
     }
 }
