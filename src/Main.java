8513535c89c034e98236deb3b2fe07ed677b4bdb
Asa Marshall <asa.marshall@spinen.com>
2020-10-06 17:31:00 -0400

    Stage uses buttons to switch scenes

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Button button;
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args); // inside the application class - set up program as javafx application
@@ -16,21 +19,27 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Title of the Window");
+        window = stage;
 
-        button = new Button();
-        button.setText("Click me");
-        button.setOnAction(e -> {
-            System.out.println("Multiple lines of code.");
-            System.out.println("Multiple lines of code.");
-        });
+        Label label1 = new Label("Welcoome to the first scene");
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        // simple layout
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        // Layout 1 - children are laid out in a vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
 
-        Scene scene = new Scene(layout, 300, 250);
-        stage.setScene(scene);
-        stage.show();
+        Button button2 = new Button("Go to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        // Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        window.setScene(scene1);
+        window.setTitle("Title here");
+        window.show();
     }
 }
