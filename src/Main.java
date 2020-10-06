2ef3937c32b5704ff5981219f875e6283d8b083e
Asa Marshall <asa.marshall@spinen.com>
2020-10-06 16:49:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args); // inside the application class - set up program as javafx application
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Title of the Window");
+
+        button = new Button();
+        button.setText("Click me");
+
+        // simple layout
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
