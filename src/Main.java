f320713edaa9982aea917011bd40948261241465
= <=>
2020-10-06 23:26:12 -0400

    Created a log in screen using GridPane

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     Stage window;
+    Button btnAlert, btnConfirm, btnClose;
 
     public static void main(String[] args) {
         launch(args); // inside the application class - set up program as javafx application
@@ -28,33 +28,42 @@ public class Main extends Application {
             closeProgram();
         });
 
-        HBox topMenu = new HBox();
-        Button buttonFile = new Button("File");
-        Button buttonEdit = new Button("Edit");
-        Button buttonView = new Button("View");
-        topMenu.getChildren().addAll(buttonFile, buttonEdit, buttonView);
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10)); // works more like a margin
+        grid.setVgap(8);
+        grid.setHgap(10);
 
-        VBox leftMenu = new VBox();
-        Button buttonD = new Button("D");
-        Button buttonE = new Button("E");
-        Button buttonF = new Button("F");
-        leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
+        // Name Label
+        Label lblName = new Label("Username:");
+        GridPane.setConstraints(lblName, 0, 0);
 
-        HBox bottomMenu = new HBox();
-        Button buttonAlert = new Button("Alert");
-        buttonAlert.setOnAction(e -> AlertBox.display("Alert", "This is an alert"));
-        Button buttonConfirm = new Button("Confirmation");
-        buttonConfirm.setOnAction(e -> ConfirmBox.display("Confirmation", "This is not really doing anything... Why did you press this?"));
-        Button buttonClose = new Button("Close");
-        buttonClose.setOnAction(e -> closeProgram());
-        bottomMenu.getChildren().addAll(buttonAlert, buttonConfirm, buttonClose);
+        // Name input
+        TextField txtName = new TextField("Bucky");
+        GridPane.setConstraints(txtName, 1, 0);
 
-        BorderPane borderPane = new BorderPane();
-        borderPane.setTop(topMenu);
-        borderPane.setLeft(leftMenu);
-        borderPane.setBottom(bottomMenu);
+        // Password Label
+        Label lblPassword = new Label("Password:");
+        GridPane.setConstraints(lblPassword, 0, 1);
 
-        Scene scene = new Scene(borderPane, 300, 250);
+        // Password input
+        TextField txtPassword = new TextField();
+        txtPassword.setPromptText("Prompt Text");
+        GridPane.setConstraints(txtPassword, 1 ,1);
+
+        // Login button
+        Button btnLogin = new Button("Log In");
+        GridPane.setConstraints(btnLogin, 1, 2);
+
+        grid.getChildren().addAll(lblName, txtName, lblPassword, txtPassword, btnLogin);
+
+        btnAlert = new Button("Alert");
+        btnAlert.setOnAction(e -> AlertBox.display("Alert", "This is an alert"));
+        btnConfirm = new Button("Confirmation");
+        btnConfirm.setOnAction(e -> ConfirmBox.display("Confirmation", "This is not really doing anything... Why did you press this?"));
+        btnClose = new Button("Close");
+        btnClose.setOnAction(e -> closeProgram());
+
+        Scene scene = new Scene(grid, 300, 200);
         window.setScene(scene);
         window.show();
     }
